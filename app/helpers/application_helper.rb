b402720d80072cd2802fb9ee72267fa0c52b9215
krongk <kenrome@gmail.com>
2013-10-23 16:05:43 +0800

    Add meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,14 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def title(page_title)
+    content_for(:title){ page_title}
+  end
+  def meta_keywords(meta_keywords)
+    content_for(:meta_keywords){ meta_keywords}
+  end
+  def meta_description(meta_description)
+    content_for(:meta_description){ meta_description}
+  end
+
 end
